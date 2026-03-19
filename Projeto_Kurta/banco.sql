4a0385fa47389ff30afd73630590036bd17bef50
Wesley Samuel <wesleysamuelfr@outlook.com>
2026-03-19 11:36:06 -0300

    Update banco.sql

--- Projeto_Kurta/banco.sql
+++ Projeto_Kurta/banco.sql
@@ -1,15 +1,20 @@
+CREATE DATABASE kurta;
+
+use kurta;
+
+
 CREATE TABLE user (
- user_id INT AUTO_INCREMENT NOT NULL,
+ user_id INT AUTO_INCREMENT PRIMARY KEY,
  first_name VARCHAR(50) NOT NULL,
  last_name VARCHAR(50),
- phone INT,
+ phone BIGINT,
  email VARCHAR(150) NOT NULL,
- birthdate DATE NOT NULL,
+ birthdate DATE,
  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE login (
-login_id INT AUTO_INCREMENT NOT NULL,
+login_id INT AUTO_INCREMENT PRIMARY KEY,
 user INT NOT NULL,
 password VARCHAR(150) NOT NULL,
 level_acess ENUM('admin', 'user') DEFAULT 'user',
@@ -18,7 +23,7 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE feeling (
-feeling_id INT AUTO_INCREMENT NOT NULL,
+feeling_id INT AUTO_INCREMENT PRIMARY KEY,
 feeling TEXT NOT NULL,
 user INT NOT NULL,
 FOREIGN KEY (user) REFERENCES user(user_id),
@@ -26,7 +31,7 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE coments (
-coment_id INT AUTO_INCREMENT NOT NULL,
+coment_id INT AUTO_INCREMENT PRIMARY KEY,
 coment TEXT,
 user INT NOT NULL,
 feeling INT NOT NULL,
@@ -36,7 +41,7 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE clan (
-clan_id INT AUTO_INCREMENT NOT NULL,
+clan_id INT AUTO_INCREMENT PRIMARY KEY,
 name_clan VARCHAR(200) NOT NULL,
 description TEXT,
 user INT NOT NULL,
@@ -47,7 +52,7 @@ created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE share (
-share_id INT AUTO_INCREMENT NOT NULL,
+share_id INT AUTO_INCREMENT PRIMARY KEY,
 share TEXT,
 user INT NOT NULL,
 feeling INT NOT NULL,
@@ -57,3 +62,11 @@ FOREIGN KEY (feeling) REFERENCES feeling(feeling_id),
 FOREIGN KEY (clan) REFERENCES clan(clan_id),
 created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
+DESCRIBE user;
+
+INSERT user (first_name, last_name, phone, email, birthdate) VALUES ('wesley', 'rodrigues', '83996312020', 'wesley@gmail.com', '2001/02/07');
+
+select * from user;
+
+ALTER TABLE user MODIFY COLUMN phone BIGINT;
